52be23bb5a5bd3804093b31bf8772a43b1004231
Peter Barclay <peter@peter-barclay.com>
2019-11-22 18:17:09 +0000

    add Friend model

---
+++ FriendFace/Friend.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let id: String
+    let name: String
+}
+
+extension Friend: Decodable {}
